4d95d9c35d26e1a96ee1356e2ffc8850190e67c1
Ricardo Espinoza <eljugadorespinoza@gmail.com>
2022-12-25 12:24:48 -0400

    Creado MVC, Empezando a pensar en las clases y como funcionara ese sistema.

---
+++ InventarioCasa/InventarioCasaModel/Articulo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventarioCasaModel
+{
+    public class Articulo
+    {
+        public string Nombre { get; set; }
+        public decimal Cantidad { get; set; }
+        public decimal Precio { get; set; }
+        public string Comprador { get; set; }//Crear Clase para esto, o buscar manera de no tener que hacer clase y tener un enum que haga la misma funcionalidad.
+        public string Vendedor { get; set; }//Crear Clase para esto, o buscar manera de no tener que hacer clase y tener un enum que haga la misma funcionalidad.
+        public DateTime FechaCompra { get; set; }
+
+    }
+}
