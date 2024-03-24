79945341552652381e30a28aeff007d539fb41a3
Ricardo Espinoza <eljugadorespinoza@gmail.com>
2024-03-24 12:57:53 -0400

    Creacion de un stopwatch que no se quita nunca de la pantalla, super simple y minimalistico. En proceso de creacion de registros para ese stopwatch, no se como hare que esos registros se vayan poniendo uno abajo o al lado del otro.

--- Cronómetro/Form1.Designer.cs
+++ Cronómetro/Form1.Designer.cs
@@ -30,135 +30,127 @@
         {
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.txtSeg = new System.Windows.Forms.TextBox();
             this.btnPlay = new System.Windows.Forms.Button();
             this.btnStop = new System.Windows.Forms.Button();
-            this.txtMin = new System.Windows.Forms.TextBox();
             this.lblxd = new System.Windows.Forms.Label();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.btnPause = new System.Windows.Forms.Button();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.btnPlus = new System.Windows.Forms.Button();
+            this.lblSeg = new System.Windows.Forms.Label();
+            this.lblMin = new System.Windows.Forms.Label();
+            this.txtDescripcion = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
-            // 
-            // txtSeg
-            // 
-            this.txtSeg.BackColor = System.Drawing.Color.Black;
-            this.txtSeg.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.txtSeg.Font = new System.Drawing.Font("Microsoft Sans Serif", 76F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)), true);
-            this.txtSeg.ForeColor = System.Drawing.SystemColors.Info;
-            this.txtSeg.Location = new System.Drawing.Point(288, 42);
-            this.txtSeg.Multiline = true;
-            this.txtSeg.Name = "txtSeg";
-            this.txtSeg.ReadOnly = true;
-            this.txtSeg.Size = new System.Drawing.Size(158, 152);
-            this.txtSeg.TabIndex = 5;
-            this.txtSeg.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // btnPlay
             // 
-            this.btnPlay.BackColor = System.Drawing.Color.Black;
+            this.btnPlay.BackColor = System.Drawing.Color.Transparent;
             this.btnPlay.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.btnPlay.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            resources.ApplyResources(this.btnPlay, "btnPlay");
             this.btnPlay.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.btnPlay.Location = new System.Drawing.Point(362, 219);
             this.btnPlay.Name = "btnPlay";
-            this.btnPlay.Size = new System.Drawing.Size(84, 33);
-            this.btnPlay.TabIndex = 6;
-            this.btnPlay.Text = "Play";
             this.btnPlay.UseVisualStyleBackColor = false;
             this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
             // 
             // btnStop
             // 
-            this.btnStop.BackColor = System.Drawing.Color.Black;
+            this.btnStop.BackColor = System.Drawing.Color.Transparent;
+            resources.ApplyResources(this.btnStop, "btnStop");
             this.btnStop.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.btnStop.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnStop.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.btnStop.Location = new System.Drawing.Point(255, 219);
             this.btnStop.Name = "btnStop";
-            this.btnStop.Size = new System.Drawing.Size(84, 33);
-            this.btnStop.TabIndex = 7;
-            this.btnStop.Text = "Stop";
             this.btnStop.UseVisualStyleBackColor = false;
             this.btnStop.Click += new System.EventHandler(this.btnPause_Click);
             // 
-            // txtMin
-            // 
-            this.txtMin.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.txtMin.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.txtMin.Font = new System.Drawing.Font("Microsoft Sans Serif", 76F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)), true);
-            this.txtMin.ForeColor = System.Drawing.SystemColors.Info;
-            this.txtMin.Location = new System.Drawing.Point(93, 42);
-            this.txtMin.Multiline = true;
-            this.txtMin.Name = "txtMin";
-            this.txtMin.ReadOnly = true;
-            this.txtMin.Size = new System.Drawing.Size(158, 152);
-            this.txtMin.TabIndex = 8;
-            this.txtMin.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            // 
             // lblxd
             // 
-            this.lblxd.AutoSize = true;
-            this.lblxd.BackColor = System.Drawing.Color.Transparent;
-            this.lblxd.Font = new System.Drawing.Font("Microsoft Sans Serif", 75.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblxd.BackColor = System.Drawing.Color.Black;
+            resources.ApplyResources(this.lblxd, "lblxd");
             this.lblxd.ForeColor = System.Drawing.Color.White;
-            this.lblxd.Location = new System.Drawing.Point(236, 59);
             this.lblxd.Name = "lblxd";
-            this.lblxd.Size = new System.Drawing.Size(77, 114);
-            this.lblxd.TabIndex = 9;
-            this.lblxd.Text = ":";
             // 
             // timer1
             // 
-            this.timer1.Interval = 1000;
+            this.timer1.Interval = 1;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // btnPause
             // 
-            this.btnPause.BackColor = System.Drawing.Color.Black;
+            this.btnPause.BackColor = System.Drawing.Color.Transparent;
             this.btnPause.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.btnPause.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnPause.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.btnPause.Location = new System.Drawing.Point(93, 219);
+            resources.ApplyResources(this.btnPause, "btnPause");
+            this.btnPause.ForeColor = System.Drawing.SystemColors.AppWorkspace;
             this.btnPause.Name = "btnPause";
-            this.btnPause.Size = new System.Drawing.Size(84, 33);
-            this.btnPause.TabIndex = 10;
-            this.btnPause.Text = "Pause";
             this.btnPause.UseVisualStyleBackColor = false;
             this.btnPause.Click += new System.EventHandler(this.btnPause_Click_1);
             // 
+            // btnPlus
+            // 
+            this.btnPlus.BackColor = System.Drawing.Color.Transparent;
+            resources.ApplyResources(this.btnPlus, "btnPlus");
+            this.btnPlus.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnPlus.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnPlus.Name = "btnPlus";
+            this.btnPlus.UseVisualStyleBackColor = false;
+            this.btnPlus.Click += new System.EventHandler(this.btnPlus_Click);
+            // 
+            // lblSeg
+            // 
+            this.lblSeg.BackColor = System.Drawing.Color.Transparent;
+            resources.ApplyResources(this.lblSeg, "lblSeg");
+            this.lblSeg.ForeColor = System.Drawing.Color.White;
+            this.lblSeg.Name = "lblSeg";
+            // 
+            // lblMin
+            // 
+            this.lblMin.BackColor = System.Drawing.Color.Transparent;
+            resources.ApplyResources(this.lblMin, "lblMin");
+            this.lblMin.ForeColor = System.Drawing.Color.White;
+            this.lblMin.Name = "lblMin";
+            // 
+            // txtDescripcion
+            // 
+            resources.ApplyResources(this.txtDescripcion, "txtDescripcion");
+            this.txtDescripcion.Name = "txtDescripcion";
+            this.txtDescripcion.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtDescripcion_KeyUp);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
-            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.ClientSize = new System.Drawing.Size(521, 277);
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
+            this.BackColor = System.Drawing.Color.Black;
+            this.Controls.Add(this.txtDescripcion);
+            this.Controls.Add(this.lblMin);
+            this.Controls.Add(this.lblSeg);
+            this.Controls.Add(this.btnPlus);
             this.Controls.Add(this.btnPause);
-            this.Controls.Add(this.txtMin);
             this.Controls.Add(this.btnStop);
             this.Controls.Add(this.btnPlay);
-            this.Controls.Add(this.txtSeg);
             this.Controls.Add(this.lblxd);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.MaximizeBox = false;
-            this.MinimumSize = new System.Drawing.Size(537, 273);
             this.Name = "Form1";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Pomodoro Timer";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.TextBox txtSeg;
         private System.Windows.Forms.Button btnPlay;
         private System.Windows.Forms.Button btnStop;
-        private System.Windows.Forms.TextBox txtMin;
         private System.Windows.Forms.Label lblxd;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Button btnPause;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Button btnPlus;
+        private System.Windows.Forms.Label lblSeg;
+        private System.Windows.Forms.Label lblMin;
+        private System.Windows.Forms.TextBox txtDescripcion;
     }
 }
 
--- Cronómetro/Form1.cs
+++ Cronómetro/Form1.cs
@@ -8,15 +8,27 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Diagnostics;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Reflection.Emit;
 
 namespace Cronómetro
 {
     public partial class Form1 : Form
     {
         Stopwatch SW1 = new Stopwatch();
+        string txtAux;
+        TimeSpan tsAux;
+        private const int cGrip = 16; // Grip size
+        private const int cCaption = 32; // Caption bar height
+        private const int threshold = 15; // Adjust threshold as needed (in pixels)
+
+
         public Form1()
         {
             InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.None; // no borders
+            this.DoubleBuffered = true;
+            this.SetStyle(ControlStyles.ResizeRedraw, true); // this is to avoid visual artifacts
         }
 
         private void btnClose_Click(object sender, EventArgs e)
@@ -34,8 +46,8 @@ namespace Cronómetro
         private void timer1_Tick(object sender, EventArgs e)
         {
             TimeSpan ts = new TimeSpan(0, 0, 0, 0, (int)SW1.ElapsedMilliseconds);
-            txtMin.Text = ts.Minutes.ToString().Length < 2 ? "0" + ts.Minutes.ToString() : ts.Minutes.ToString();
-            txtSeg.Text = ts.Seconds.ToString().Length < 2 ? "0" + ts.Seconds.ToString() : ts.Seconds.ToString();
+            lblMin.Text = ts.Minutes.ToString().Length < 2 ? "0" + ts.Minutes.ToString() : ts.Minutes.ToString();
+            lblSeg.Text = ts.Seconds.ToString().Length < 2 ? "0" + ts.Seconds.ToString() : ts.Seconds.ToString();
         }
 
         private void btnPause_Click(object sender, EventArgs e)
@@ -47,5 +59,103 @@ namespace Cronómetro
         {
             SW1.Stop();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Size = new Size(107, 52);
+        }
+
+
+        protected override void WndProc(ref Message m)
+        {
+            switch (m.Msg)
+            {
+                case 0x84:
+                    base.WndProc(ref m);
+                    if ((int)m.Result == 0x1)
+                        m.Result = (IntPtr)0x2;
+                    return;
+            }
+
+            base.WndProc(ref m);
+        }
+        private Point startPoint;
+
+        private void btnPlus_Click(object sender, EventArgs e)
+        {
+            this.Size = new Size(316, 25);
+            txtDescripcion.Visible = true;
+            
+        }
+
+        private void txtDescripcion_KeyUp(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+            {
+                tsAux = SW1.Elapsed;
+                txtAux = txtDescripcion.Text;
+                this.Size = new Size(107, 52);
+                txtDescripcion.Clear();
+                txtDescripcion.Visible = false;
+            }
+        }
+
+        //private void lblxd_MouseDown(object sender, MouseEventArgs e)
+        //{
+        //    if (e.Button == MouseButtons.Left)
+        //    {
+        //        isDragging = true;
+        //        startPoint = Cursor.Position;
+        //        dragTimer.Start();
+        //    }
+        //}
+
+        //private void lblxd_MouseMove(object sender, MouseEventArgs e)
+        //{
+        //    if (isDragging)
+        //    {
+        //        int deltaX = Cursor.Position.X - startPoint.X;
+        //        int deltaY = Cursor.Position.Y - startPoint.Y;
+        //        Location = new Point(Location.X + deltaX, Location.Y + deltaY);
+        //    }
+        //}
+
+        //private void lblxd_MouseUp(object sender, MouseEventArgs e)
+        //{
+        //    isDragging = false;
+        //    dragTimer.Stop();
+        //}
+        //private void DragTimer_Tick(object sender, EventArgs e)
+        //{
+        //    if (isDragging)
+        //    {
+        //        int deltaX = Cursor.Position.X - startPoint.X;
+        //        int deltaY = Cursor.Position.Y - startPoint.Y;
+
+        //        // Apply threshold check
+        //        if (Math.Abs(deltaX) > threshold || Math.Abs(deltaY) > threshold)
+        //        {
+        //            // Calculate scaled movement based on threshold and factor
+        //            int movementX = (int)(Math.Sign(deltaX) * Math.Max(threshold, Math.Abs(deltaX)) * 0.1);
+        //            int movementY = (int)(Math.Sign(deltaY) * Math.Max(threshold, Math.Abs(deltaY)) * 0.1);
+
+        //            // Restrict movement to prevent the form from going off-screen
+        //            int newX = Location.X + movementX;
+        //            int newY = Location.Y + movementY;
+
+        //            if (newX >= 0 && newX + Width <= Screen.PrimaryScreen.WorkingArea.Width)
+        //            {
+        //                Location = new Point(newX, Location.Y);
+        //            }
+
+        //            if (newY >= 0 && newY + Height <= Screen.PrimaryScreen.WorkingArea.Height)
+        //            {
+        //                Location = new Point(Location.X, newY);
+        //            }
+        //        }
+        //    }
+        //}
     }
 }
+
+
